f0c04ae702c01aa4c97abd1f9d01e8b714d57d3b
SawanThapa25056 <kv20061126@gmail.com>
2026-04-04 18:18:40 +0530

    Day-9
    Mirror the code name.

---
+++ Q9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char str[], int index) {
+    if (str[index] == '\0')
+        return;
+
+    reverse(str, index + 1);
+    printf("%c", str[index]);
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter code name: ");
+    scanf("%s", str);
+
+    printf("Mirror code: ");
+    reverse(str, 0);
+
+    return 0;
+}
