4fa2f96d4fc591c8cbbf73e697539af0b3b6eabe
SawanThapa25056 <kv20061126@gmail.com>
2026-04-06 22:59:31 +0530

    Day-11
    Matrix addition

---
+++ Q11.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+
+    scanf("%d %d", &m, &n);
+
+    int A[m][n], B[m][n], result[m][n];
+
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            result[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
